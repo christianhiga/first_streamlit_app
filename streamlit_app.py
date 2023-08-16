6fc613cf30db0ad507a841f31cfba0d91518dc13
Christian Higa <higachristian@gmail.com>
2023-08-15 23:47:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
-streamlit.header("Breakfast Menu")
-streamlit.text("asdasd")
-streamlit.text("111111")
-streamlit.text("222222")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
