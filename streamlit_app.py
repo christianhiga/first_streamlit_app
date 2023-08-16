1bdd9ab8368437cc0cc64be7674193927bb4cb27
Christian Higa <higachristian@gmail.com>
2023-08-15 23:46:53 -0300

    Update streamlit_app.py
    Adding header and text as example.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("asdasd")
+streamlit.text("111111")
+streamlit.text("222222")
